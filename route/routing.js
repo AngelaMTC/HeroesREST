936e84b26fc0f869b81299bde6bf0941fc1a2b21
AngelaTC <tc.angela.m@gmail.com>
2020-12-15 17:35:34 -0600

    Servidor REST de Heroes.

---
+++ route/routing.js
@@ -0,0 +1,39 @@
+const heroes = require("../controller/heroes.controller.js");
+var router = require("express").Router();
+
+// GET Methods
+
+// GET All
+router.get("/heroes", heroes.findAll);
+
+// GET by ID
+router.get("/heroe/:id", heroes.findOne);
+
+// // GET by Term
+router.get("/heroesTerm", heroes.findSome);
+
+// // GET Active
+router.get("/heroesAct", heroes.findActive);
+
+// GET Agrupación(aggregate):
+router.get('/heroesGroup', heroes.grouping);
+
+// POST Heroe
+router.post("/heroe",heroes.create);
+
+/** POST Insertar Heroe:**/
+router.post('/InsH', heroes.insertarHeroe);
+
+/**POST Actualizar Heroe**/ 
+router.post('/ActH', heroes.actualizarHeroe);
+
+/**DELETE Eliminar Heroe**/ 
+router.post('/EliH', heroes.eliminarHeroe);
+
+// DELETE Heroe:
+router.delete("/hero/:id", heroes.delete);
+
+// PUT Heroe:
+router.put('/heroe/:id', heroes.update);
+
+module.exports = router;
